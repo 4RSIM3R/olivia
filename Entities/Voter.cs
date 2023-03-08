98ce158fd05d29c3953718ee43e38f798ac58cc3
MuhammadHatta72 <attah1335@gmail.com>
2023-03-08 21:03:12 +0700

    fix: name NIM become SIN

--- Entities/Voter.cs
+++ Entities/Voter.cs
@@ -3,7 +3,7 @@ namespace Olivia.Entites;
 class Voter
 {
     public int Id { get; set; }
-    public string NIM { get; set; }
+    public string SIN { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
