8f8634fb6ec42afcb31f705fa1b60e3fbd17fef6
argyaWicaksana <argya7@gmail.com>
2023-03-08 21:00:17 +0700

    add tenant class

---
+++ Entities/Tenant.cs
@@ -0,0 +1,9 @@
+namespace Olivia.Entites;
+
+class Tenant
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
