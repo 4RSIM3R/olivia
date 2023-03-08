fe55e78caefc00ed9ea9ba176979d35e82e15ed5
MuhammadHatta72 <attah1335@gmail.com>
2023-03-08 20:57:11 +0700

    feat: add class voter

---
+++ Entities/Voter.cs
@@ -0,0 +1,14 @@
+namespace Olivia.Entites;
+
+class Voter
+{
+    public int Id { get; set; }
+    public string NIM { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
+    public string Major { get; set; }
+
+}
