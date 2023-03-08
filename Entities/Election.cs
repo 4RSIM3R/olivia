4743b9d7f98c68e5360fbcaf6e8a26ee57035816
FaruqAlfa <alfafahmi172@gmail.com>
2023-03-08 21:29:38 +0700

    Update Election

---
+++ Entities/Election.cs
@@ -0,0 +1,18 @@
+namespace Olivia.Entites;
+
+class Election{
+
+    public int Id { get; set; }
+
+    public DateTime  StartAdd { get; set; }
+
+    public DateTime EndAdd { get; set; }
+
+    public Tenant Tenant { get; set; }
+
+
+
+
+
+
+}
