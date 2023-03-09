443ea27094879639f38eeff4160f00e3dd9639a7
FaruqAlfa <alfafahmi172@gmail.com>
2023-03-09 10:35:52 +0700

    update Election

--- Entities/Election.cs
+++ Entities/Election.cs
@@ -4,9 +4,9 @@ class Election{
 
     public int Id { get; set; }
 
-    public DateTime  StartAdd { get; set; }
+    public DateTime  StartAt { get; set; }
 
-    public DateTime EndAdd { get; set; }
+    public DateTime EndAt { get; set; }
 
     public Tenant Tenant { get; set; }
 
