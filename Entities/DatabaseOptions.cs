8174608b1a271dc209c5f2c1dc7bb200c6737db9
elianiva <dicha.arkana03@gmail.com>
2023-03-18 15:05:48 +0700

    feat: use option pattern for database config

---
+++ Entities/DatabaseOptions.cs
@@ -0,0 +1,9 @@
+namespace Olivia.Entities;
+
+public sealed class DatabaseOptions
+{
+    public string Server { get; set; } = string.Empty;
+    public string Database { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,22 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Olivia.Entites;
+using Olivia.Entities;
 using Olivia.Repositories;
 using Olivia.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DBConnection");
+// add database options
+builder.Services.Configure<DatabaseOptions>(builder.Configuration.GetSection("DatabaseOptions"));
+
+// configure swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add services to the container.
-builder.Services.AddDbContextPool<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddApplicationDbContext();
 builder.Services.AddOliviaRepositories();
 builder.Services.AddOliviaServices();
 
